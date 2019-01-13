65c1b618ceda43bfaf4437e99d6a97d54ccdef0c
Isleimar Oliveira <isleimar@hotmail.com>
2019-01-13 19:59:28 -0300

    Metodos iniciais dos Testes classe PrecoDAONemTeste

---
+++ app/AutoParking/src/test/java/br/com/ifpb/teste/auto_parking/PrecoDAOMemTeste.java
@@ -0,0 +1,39 @@
+package br.com.ifpb.teste.auto_parking;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.com.ifpb.teste.auto_parking.dao.imp.PrecoDAOMem;
+import br.com.ifpb.teste.auto_parking.model.Preco;
+
+public class PrecoDAOMemTeste {
+	
+	private static ArrayList<Preco> listaPreco = new ArrayList<Preco>();
+	private PrecoDAOMem precoDAO = null;
+	
+	@BeforeClass
+	public static void criarPrecos() {
+		listaPreco.add(new Preco(true, 0, 30, 5.5f));   //     0 ~ 30  minutos
+		listaPreco.add(new Preco(true, 31, 60, 4.4f));  //    31 ~ 60  minutos
+		listaPreco.add(new Preco(true, 61, 120, 4.0f)); //    61 ~ 120 minutos
+		listaPreco.add(new Preco(true, 121, 180, 3.6f));//   121 ~ 180 minutos
+		listaPreco.add(new Preco(false, 181, 0, 0.0583f));// 181 ~ ... minutos - Relativo aos minutos
+	}
+	
+	@Before
+	public void inicioTeste() {
+		precoDAO = new PrecoDAOMem();
+		for(Preco preco: listaPreco) {
+			precoDAO.cadastrarPreco(preco);
+		}
+	}
+
+}
