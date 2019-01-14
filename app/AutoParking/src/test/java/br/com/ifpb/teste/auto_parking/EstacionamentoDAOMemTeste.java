d5bc9c500545ff7a3df2d90e2ef0bf003e84105a
Isleimar <isleimar@gmail.com>
2019-01-14 11:21:10 -0300

    Metodo criarEstacionemtno da classe EstacionamentoDAOMemTeste

--- app/AutoParking/src/test/java/br/com/ifpb/teste/auto_parking/EstacionamentoDAOMemTeste.java
+++ app/AutoParking/src/test/java/br/com/ifpb/teste/auto_parking/EstacionamentoDAOMemTeste.java
@@ -1,5 +1,6 @@
 package br.com.ifpb.teste.auto_parking;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 import org.junit.BeforeClass;
@@ -13,7 +14,11 @@ public class EstacionamentoDAOMemTeste {
 	private EstacionamentoDAOMem estacionamentoDAO = null;
 	
 	@BeforeClass
-	public static void criarEstacionamentos() {		
+	public static void criarEstacionamentos() {
+		//listaEstacionamento.add(new Estacionamento(id, quitado, finalizado, placa, dhChegada, dhPagamento, dhSaida, valor));
+		listaEstacionamento.add(new Estacionamento(1, true, true, "JJK-1373", LocalDateTime.now().minusHours(1), LocalDateTime.now().minusMinutes(30), LocalDateTime.now(), 5.5));
+		listaEstacionamento.add(new Estacionamento(2, false, false, "JOE-8330", LocalDateTime.now().minusHours(1), null, null, null));
+		listaEstacionamento.add(new Estacionamento(3, true, false, "EFH-9373", LocalDateTime.now().minusHours(2), LocalDateTime.now().minusMonths(1), LocalDateTime.now(), 5.5));
 	}
 
 }
