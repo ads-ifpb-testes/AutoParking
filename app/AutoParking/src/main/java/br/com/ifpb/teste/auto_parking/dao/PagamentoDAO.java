b1ca1c39402b89c2a2c168be7fa5821d76f064c4
Isleimar <isleimar@gmail.com>
2019-01-16 10:00:06 -0300

    Modificação da interface PagamentoDAO

--- app/AutoParking/src/main/java/br/com/ifpb/teste/auto_parking/dao/PagamentoDAO.java
+++ app/AutoParking/src/main/java/br/com/ifpb/teste/auto_parking/dao/PagamentoDAO.java
@@ -1,11 +1,23 @@
 package br.com.ifpb.teste.auto_parking.dao;
 
+import java.util.List;
+
+import br.com.ifpb.teste.auto_parking.model.Pagamento;
+
 public interface PagamentoDAO {
 	
+	boolean cadastrarPagamento(Pagamento pagamento);
+	boolean editarPagamento(Pagamento pagamento);
+	boolean excluirPagamento(Pagamento pagamento);
+	List<Pagamento> buscarPagamento(int id);
+	List<Pagamento> buscarPagamentoCpf(String  cpf);
+	List<Pagamento> buscarPagamentoPlaca(String  placa);	
+	List<Pagamento> listarPagamento();
 	void gerarValor(int id);
 	double valorDevido(int id);
 	double calcularTroco(int id, double valorPago);
 	void registrarPagamento(int id);	
 	void incluirCredito(double valor);
+	
 
 }
